1e03e4530cace8160b9eb0a799ec826250722dbc
Vadim Shashin <vashashin@avito.ru>
2016-10-26 00:09:35 +0300

    typos

--- core/core.go
+++ core/core.go
@@ -4,6 +4,7 @@ import "gopkg.in/mgo.v2"
 
 const (
 	SchemaCollection = "schemes"
+	SchemaStorage = "schema_storage"
 )
 
 type Core struct {
@@ -16,7 +17,7 @@ func NewCore(mongo *mgo.Session) *Core {
 }
 
 func (c *Core) GetDB() *mgo.Database {
-	return c.Mongo.DB("schema_storage")
+	return c.Mongo.DB(SchemaStorage)
 }
 
 func (c *Core) GetCollection(Name string) *mgo.Collection {
